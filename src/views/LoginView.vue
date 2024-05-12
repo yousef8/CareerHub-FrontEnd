665d4710e3c7d49ec3d055a667ca897b0565e03f
Steven Adel <stevenadel.dev@gmail.com>
2024-05-12 15:29:38 +0300

    Add login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,56 @@
 <template>
-    <h1>Login works!</h1>
+    <div class="login-container d-flex align-items-center">
+        <div class="login-form container p-3 text-dark bg-white">
+            <h1 class="my-4 text-center fw-semibold">Welcome Back!</h1>
+            <p class="text-center">Didn't join CareerHub yet? <router-link to="/register">Sign up here</router-link></p>
+            <form @submit.prevent="handleSubmit" class="d-flex flex-column align-items-center">
+                <div class="mb-3 w-75">
+                    <label for="email" class="form-label">Email</label>
+                    <input v-model="email" type="email" id="email" class="form-control" required>
+                </div>
+                <div class="my-1 w-75">
+                    <label for="password" class="form-label">Password</label>
+                    <input v-model="password" type="password" id="password" class="form-control" pattern=".{8,}" title="Password must be at least 8 characters long" required>
+                </div>
+                <button type="submit" class="btn btn-primary my-4 w-25 fs-5">Login</button>
+            </form>
+        </div>
+    </div>
+
 </template>
 
 <script>
 export default {
     data() {
         return {
-            
-        };
+            email: '',
+            password: '',
+        }
     },
     methods: {
-        
+        handleSubmit() {
+            alert(`Email: ${this.email}, Password: ${this.password}`);
+            this.email = '';
+            this.password = '';
+        }
     }
-};
+}
 </script>
 
 <style scoped>
+.login-container {
+    height: 100vh;
+    background-color: #ebebeb !important;
+    background: radial-gradient(circle at 0px 0px, rgb(18, 70, 149), rgb(0, 112, 235)) 0px 0px / 100% 460px no-repeat;
+}
 
+.login-form {
+    border-radius: 10px !important;
+}
+
+@media (max-width: 576px) {
+    .btn {
+        width: 80% !important;
+    }
+}
 </style>
