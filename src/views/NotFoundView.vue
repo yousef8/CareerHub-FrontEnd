0fe3a0f0d2b5d0ded7f3de79d2c7a37354878ce0
Steven Adel <stevenadel.dev@gmail.com>
2024-05-12 19:15:10 +0300

    Add not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,20 +1,7 @@
 <template>
-    <h1 class="text-danger text-center">404 Page Not Found!</h1>
+    <div class="vh-100 d-flex flex-column justify-content-center">
+        <h1 class="text-center text-danger my-4">Page Not Found</h1>
+        <h3 class="text-center text-secondary">The requested page does not exist, please check the entered URL</h3>
+        <img class="mx-auto d-block" src="../../public/not-found.jpg" alt="not found image">
+    </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            
-        };
-    },
-    methods: {
-        
-    }
-};
-</script>
-
-<style scoped>
-
-</style>
