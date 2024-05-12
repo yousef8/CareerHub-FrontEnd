bb3df44f6c223b3d8261c73d47679e4b3bc0d2e9
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-12 07:57:34 +0300

    use pagination in job view

--- src/views/candidate/JobsView.vue
+++ src/views/candidate/JobsView.vue
@@ -5,24 +5,45 @@
     <div v-else>
       <div v-if="jobs.length === 0" class="text-center">No jobs available</div>
       <div v-else>
-        <JobCard v-for="(job, index) in jobs" :key="index" :job="job" />
+        <JobCard v-for="(job, index) in visibleJobs" :key="index" :job="job" />
       </div>
+      <PaginationComponent
+        :current-page="currentPage"
+        :total-pages="totalPages"
+        :prev-page="prevPage"
+        :next-page="nextPage"
+        :goto-page="gotoPage"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import JobCard from '@/components/JobCard.vue';
+import PaginationComponent from '@/components/PaginationComponent.vue'; 
 
 export default {
   components: {
-    JobCard
+    JobCard,
+    PaginationComponent
   },
   data() {
     return {
       jobs: [],
-      loading: false
+      loading: false,
+      currentPage: 1,
+      pageSize: 2 // Change this value to adjust the number of jobs per page
     };
+  },
+  computed: {
+    visibleJobs() {
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      return this.jobs.slice(startIndex, endIndex);
+    },
+    totalPages() {
+      return Math.ceil(this.jobs.length / this.pageSize);
+    }
   },
   mounted() {
     this.fetchJobs();
@@ -39,7 +60,24 @@ export default {
       } finally {
         this.loading = false;
       }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    gotoPage(pageNumber) {
+      this.currentPage = pageNumber;
     }
   }
 };
 </script>
+
+<style>
+/* Add custom styles here if needed */
+</style>
