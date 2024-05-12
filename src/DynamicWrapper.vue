f6ecd5dee3e9f134776ad70aed959ad62391fca6
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-12 07:14:45 +0300

    add dynamic component

---
+++ src/DynamicWrapper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="row g-0 m-5">
+      <ul class="breadcrumb py-2 bg-body-secondary rounded table-light w-50 m-auto justify-content-around p3 text-center">
+        <li class="breadcrumb-item"><a href="#" class="text-dark text-decoration-none fs-4 fw-bold" @click="targetcomponent='profileComponent'">Profile</a></li>
+        <li class="breadcrumb-item"><a href="#" class="text-dark text-decoration-none fs-4 fw-bold" @click="targetcomponent='applicationComponent'">Application</a></li>
+      </ul>
+    </div>
+    <div class="row g-0 text-center w-50 m-auto">
+    <keep-alive>
+      <component :is="targetcomponent" />
+    </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import profileComponent from './components/dynamicComponent/profileComponent';
+import applicationComponent from './components/dynamicComponent/applicationComponent';
+
+export default {
+  data() {
+    return {
+      targetcomponent: 'profileComponent'
+    }
+  },
+
+  components: {
+    profileComponent,
+    applicationComponent
+  }
+}
+</script>
+
+<style>
+/* Add your styles here if needed */
+</style>
