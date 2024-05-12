ac6b5901d1c4a79f299e257f74da5963da88bb9b
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-12 07:30:27 +0300

    add application component

--- src/views/candidate/dynamicwrapper.vue
+++ src/views/candidate/dynamicwrapper.vue
@@ -15,8 +15,8 @@
 </template>
 
 <script>
-import profileComponent from './views/candidate/profileComponent';
-import applicationComponent from './components/dynamicComponent/applicationComponent';
+import profileComponent from './views/candidate/profileComponent.vue';
+import applicationComponent from './views/candidate/applicationComponent.vue';
 
 export default {
   data() {
