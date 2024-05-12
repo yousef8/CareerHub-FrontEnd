81ff09ef433bf8ababfebe6b0cb0d572e61555ca
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-12 07:40:52 +0300

    handle profile

--- src/views/candidate/profileComponent.vue
+++ src/views/candidate/profileComponent.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="mb-5 border border-3 p-4  border-secondary rounded">
+  <div class="mb-5 border border-3 p-4 border-secondary rounded">
     <h2 class="text-center">User Profile</h2>
     <div class="card">
-      <div class="card-body d-flex align-items-center">
-        <div class="user-image me-3">
+      <div class="card-body row align-items-center">
+        <div class="col-md-auto mb-3 mb-md-0">
           <img src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png" alt="User Image" class="img-fluid rounded-circle" style="max-width: 150px;">
         </div>
-        <div class="user-info flex-grow-1">
+        <div class="col-md">
           <div v-for="(info, key) in user" :key="key" class="mb-3">
             <div class="d-flex justify-content-between">
-              <div v-if="!editing[key]">
+              <div v-if="!editing[key]" class="flex-grow-1">
                 <span class="fw-bold">{{ key }}:</span>
                 <span>{{ info }}</span>
               </div>
@@ -63,9 +63,5 @@ export default {
 /* You can add custom styles here if needed */
 .user-image {
   flex-shrink: 0; /* Prevent the image from shrinking */
-}
-
-.user-info {
-  width: 100%;
 }
 </style>
