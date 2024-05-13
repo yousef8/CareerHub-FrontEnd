13ad884f36d68dd2de707250d7bcca7c4348a111
Yousef Elsayed <yousef4511@outlook.com>
2024-05-13 20:54:08 +0300

    Implement logout logic

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,11 +1,24 @@
 <template>
-  <nav class="navbar navbar-expand-lg px-2 m-0 " style=" background-color: #2784FC;">
-    <a class="navbar-brand fs-1 ms-2 me-lg-5 fw-bold text-white" style="font-family: 'Dancing Script', cursive;" href="#">CareerHub</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg px-2 m-0" style="background-color: #2784fc">
+    <a
+      class="navbar-brand fs-1 ms-2 me-lg-5 fw-bold text-white"
+      style="font-family: 'Dancing Script', cursive"
+      href="#"
+      >CareerHub</a
+    >
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="navbar-nav me-auto  ">
+      <div class="navbar-nav me-auto">
         <a class="nav-item nav-link fs-4 me-lg-4 rounded-5">
           <router-link to="/" class="text-decoration-none text-white">Home</router-link>
         </a>
@@ -16,24 +29,38 @@
           <router-link to="/" class="text-decoration-none text-white">Employers</router-link>
         </a>
       </div>
-      <form class="d-flex mx-auto mx-lg-0 w-100 w-lg-auto mt-2"> <!-- Centered search bar -->
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <form class="d-flex mx-auto mx-lg-0 w-100 w-lg-auto mt-2">
+        <!-- Centered search bar -->
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         <button class="btn btn-outline-dark me-3 fw-bold text-white" type="submit">Search</button>
       </form>
-      <div class="navbar-nav ms-auto fs-4  mt-2">
-        <a class="nav-item nav-link fs-4 me-4 p-2 rounded-5 ">
+      <div class="navbar-nav ms-auto fs-4 mt-2">
+        <a class="nav-item nav-link fs-4 me-4 p-2 rounded-5">
           <router-link to="/login" class="text-decoration-none text-white">Login</router-link>
         </a>
-        <a class="nav-item nav-link fs-4 me-4 p-2 rounded-5 ">
+        <a class="nav-item nav-link fs-4 me-4 p-2 rounded-5">
           <router-link to="/register" class="text-decoration-none text-white">Register</router-link>
         </a>
         <li class="nav-item dropdown rounded-3">
-          <a class=" nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a
+            class="nav-link dropdown-toggle text-white"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
             Dropdown
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <router-link to="/my" class="text-decoration-none text-white"><li><a class="dropdown-item fs-5" href="#">Profile</a></li></router-link>
-            <li><a class="dropdown-item fs-5" href="#">Logout</a></li>
+            <router-link
+              :to="{ name: 'profile-applications' }"
+              class="text-decoration-none text-white"
+              ><li><a class="dropdown-item fs-5" href="#">Profile</a></li></router-link
+            >
+            <router-link :to="{ name: 'logout' }" class="text-decoration-none text-white"
+              ><li><a class="dropdown-item fs-5">Logout</a></li></router-link
+            >
           </ul>
         </li>
         <i class="fa-regular fa-circle-user fs-2 m-3"></i>
@@ -43,7 +70,7 @@
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style>
