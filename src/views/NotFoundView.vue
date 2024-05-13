b9969ae212939f4ac62d14f7223a318f1cb81a77
Steven Adel <stevenadel.dev@gmail.com>
2024-05-13 15:22:05 +0300

    Styling tweaks for not found page for better responsivity

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="vh-100 d-flex flex-column justify-content-center">
         <h1 class="text-center text-danger my-4">Page Not Found</h1>
-        <h3 class="text-center text-secondary">The requested page does not exist, please check the entered URL</h3>
-        <img class="mx-auto d-block" src="../assets/images/not-found.jpg" alt="page not found">
+        <h3 class="text-center text-secondary px-5">The requested page does not exist, please check the entered URL</h3>
+        <img class="mx-auto d-block img-fluid" src="../assets/images/not-found.jpg" alt="page not found">
     </div>
 </template>
