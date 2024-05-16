5dc3b59d6f1db2b0a0d704221b3f8cf5155aae89
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-16 12:36:23 +0300

    Application integration for candidate

--- src/components/applicationComponent.vue
+++ src/components/applicationComponent.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mb-5 border border-3 p-4 border-secondary rounded">
     <h2 class="text-center">Applications</h2>
-    <div v-if="userApplications.length === 0" class="text-center">No applications found.</div>
+    <div v-if="applications.length === 0" class="text-center">No applications found.</div>
     <div v-else>
-      <div v-for="(application, index) in userApplications" :key="index" class="card mb-3" :style="{ backgroundColor: getStatusColor(application.status) }">
+      <div v-for="(application, index) in applications" :key="index" class="card mb-3" :style="{ backgroundColor: getStatusColor(application.status) }">
         <div class="card-body">
           <h5 class="card-title">Application ID: {{ application.id }}</h5>
           <p class="card-text" >Status: {{ application.status }}</p>
@@ -19,43 +19,42 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      userApplications: []
+      applications: []
     };
   },
   created() {
-  const accessToken = localStorage.getItem('token');
-  const userId = localStorage.getItem('id'); // Corrected from 'user.id'
+    const accessToken = localStorage.getItem('token');
 
-  if (accessToken && userId) {
-    axios.get('http://localhost:8000/api/applications/', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    })
-    .then(response => {
-      // Filter applications for the logged-in user
-      this.userApplications = response.data.filter(application => application.user_id === parseInt(userId));
-    })
-    .catch(error => {
-      console.error('Error fetching applications:', error);
-    });
-  }
-},
+    if (accessToken) {
+      axios.get('http://localhost:8000/api/applications/', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      .then(response => {
+        this.applications = response.data;
+      })
+      .catch(error => {
+        console.error('Error fetching applications:', error);
+      });
+    }
+  },
 
   methods: {
-    getStatusColor(status) {
-      switch (status) {
-        case 'Pending':
-          return 'yellow';
-        case 'Accepted':
-          return 'green';
-        case 'Rejected':
-          return 'red';
-        default:
-          return 'white'; 
-      }
+  getStatusColor(status) {
+    switch (status.toLowerCase()) {
+      case 'pending':
+        return 'yellow';
+      case 'accepted':
+        return 'green';
+      case 'rejected':
+        return 'red';
+      default:
+        return 'white'; 
     }
   }
+}
+
 };
 </script>
 
