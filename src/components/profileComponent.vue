7a112cad3d40d67c8383d36193c11381dee6c928
fatmaelhot <fatmaelhot289@gmail.com>
2024-05-17 17:46:19 +0300

    update profile functionality

--- src/components/profileComponent.vue
+++ src/components/profileComponent.vue
@@ -3,13 +3,17 @@
     <h2 class="text-center"><span class="text-primary">Hello</span> {{ user.name }}</h2>
     <div class="card">
       <div class="card-body row align-items-center">
-        <div class="col-md-auto col-sm-12 mb-3 mb-md-0 text-center m-auto">
+         <div class="col-md-auto col-sm-12 mb-3 mb-md-0 text-center m-auto">
           <img :src="user.profile_image ? user.profile_image : 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png'" alt="User Image" class="img-fluid rounded-circle mb-4" style="max-width: 150px;">
+
+          <label for="imageUpload" class="btn btn-link">
+            <i class="fas fa-camera fa-2x text-primary"></i>
+            <input type="file" id="imageUpload" style="display: none" @change="handleImageChange">
+          </label>
         </div>
         <div class="col-md-12 col-sm-12"> 
-          <div v-if="editing.name || editing.phone" class="mb-3">
+          <div v-if="editing.name" class="mb-3">
             <input type="text" v-model="user.name" class="form-control mb-2" placeholder="Name">
-            <input type="text" v-model="user.phone_number" class="form-control mb-2" placeholder="Phone">
             <button @click="updateProfile('name')" class="btn btn-primary mb-2">Save</button>
           </div>
           <div v-else>
@@ -20,6 +24,12 @@
                 <button @click="startEditing('name')" class="btn btn-link"><i class="fas fa-pen fs-5 text-primary"></i></button>
               </div>
             </div>
+          </div>
+          <div v-if="editing.email" class="mb-3">
+            <input type="email" v-model="user.email" class="form-control mb-2" placeholder="Email">
+            <button @click="updateProfile('email')" class="btn btn-primary mb-2">Save</button>
+          </div>
+          <div v-else>
             <div class="row">
               <div class="mb-3 col">
                 <div class="d-flex justify-content-between align-items-center">
@@ -29,6 +39,12 @@
                 </div>
               </div>
             </div>
+          </div>
+          <div v-if="editing.phone" class="mb-3">
+            <input type="text" v-model="user.phone_number" class="form-control mb-2" placeholder="Phone">
+            <button @click="updateProfile('phone_number')" class="btn btn-primary mb-2">Save</button>
+          </div>
+          <div v-else>
             <div class="mb-3">
               <div class="d-flex justify-content-between align-items-center">
                 <div class="fw-bold text-primary">Phone:</div>
@@ -37,6 +53,8 @@
               </div>
             </div>
           </div>
+         
+          
           <div v-if="updateSuccess" class="alert alert-success mt-3" role="alert">
             Profile updated successfully!
           </div>
@@ -47,6 +65,8 @@
 </template>
 
 <script>
+import api from '@/utilities/axios'; 
+
 export default {
   data() {
     return {
@@ -54,7 +74,8 @@ export default {
       editing: {
         name: false,
         email: false,
-        phone: false
+        phone: false,
+        image: false
       },
       updateSuccess: false
     };
@@ -67,14 +88,30 @@ export default {
   },
   methods: {
     startEditing(key) {
-      this.editing = { ...this.editing, [key]: true };
+      
+      Object.keys(this.editing).forEach(editKey => {
+        this.editing[editKey] = editKey === key;
+      });
     },
-    updateProfile(key) {
-      this.editing = { ...this.editing, [key]: false };
-      this.updateSuccess = true;
-      setTimeout(() => {
-        this.updateSuccess = false;
-      }, 3000); 
+    async updateProfile(key) {
+      try {
+        const response = await api.put('/user', { [key]: this.user[key] });
+        console.log('Profile updated successfully:', response.data);
+        this.editing[key] = false; 
+        this.updateSuccess = true;
+        setTimeout(() => {
+          this.updateSuccess = false;
+        }, 3000);
+      } catch (error) {
+        console.error('Error updating profile:', error);
+       
+      }
+    },
+    handleImageChange(event) {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('profile_image', file);
+      this.updateProfile(formData);
     }
   }
 };
